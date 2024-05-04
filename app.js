bdb39231abaf7728c7f207d590f2ba6e98d8ea33
cardanes <cardanesrabota@gmail.com>
2024-05-04 16:04:17 +0300

    Добавление

--- app.js
+++ app.js
@@ -2,19 +2,27 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const mainRoute = require("./routes/main");
+// const mainRoute = require("./routes/main");
 const gamesRouter = require("./routes/games");
+const usersRouter = require('./routes/users');
+const categoriesRouter = require('./routes/categories');
 const { cors } = require("./middlewares/cors")
+
+const connectToDatabase = require('./database/connect');
 
 const PORT = 3000;
 const app = express();
+
+connectToDatabase();
 
 // Теперь клиент имеет доступ только к публичным файлам
 app.use(
   cors,
   bodyParser.json(),
   express.static(path.join(__dirname, "public")),
-  mainRoute, 
+  usersRouter, 
+  categoriesRouter,
+  // mainRoute, 
   gamesRouter
 );
 
