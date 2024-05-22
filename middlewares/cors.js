4e0e828c82d1eff9523c5e667687ee7b773538a4
cardanes <cardanesrabota@gmail.com>
2024-05-22 21:47:25 +0300

    CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,29 +1,23 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000',
-    'http://localhost:3001'
+  "roobi-frontend.nomoredomainswork.ru",
+  "http://localhost:3000",
+  "http://localhost:3001",
 ];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
+  const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Добавляем разрешенные методы
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Добавляем разрешенные заголовки
-    }
-
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200); // Отправляем успешный ответ на предварительный запрос
-    } else {
-        next();
-    }
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
+  next();
 }
 
 module.exports = {
-    cors
+  cors,
 };
